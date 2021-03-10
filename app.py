cc17267e66eff2d94381f134b8350d275814dd28
willpcarnevale <80362812+willpcarnevale@users.noreply.github.com>
2021-03-10 15:03:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,19 +18,19 @@ def profile(name):
 	return render_template('index.html', name=name)
 
 
-@app.route('/add_numbers', methods=['GET','POST'])
+@app.route('/multiply_numbers', methods=['GET','POST'])
 def add_numbers_post():
 	  # --> ['5', '6', '8']
 	  # print(type(request.form['text']))
 	  if request.method == 'GET':
-	  	return render_template('add_numbers.html')
+	  	return render_template('multiply_numbers.html')
 	  elif request.method == 'POST':
   	      print(request.form['text'].split())
   	      total = 0
   	      try:
   	      	for str_num in request.form['text'].split():
-  	      		total += int(str_num)
-  	      	return render_template('add_numbers.html', result=str(total))
+  	      		total *= int(str_num)
+  	      	return render_template('multiply_numbers.html', result=str(total))
   	      except ValueError:
   	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
 
